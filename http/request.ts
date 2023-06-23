92a13be551001d19962af33d80973c91c579343a
ianpatricck <ianpatrick499@gmail.com>
2023-06-22 22:21:16 -0300

    refactor: body and params are changed to type 'any'

--- http/request.ts
+++ http/request.ts
@@ -1,18 +1,13 @@
 import { IncomingMessage } from 'http';
 
-type ObjectSanitized = {
-    [key: string]: string | boolean | number | null | undefined;
-}
-
 type Request = {
     method: string;
     path: string | undefined;
-    params: ObjectSanitized;
-    body: ObjectSanitized;
+    params: any;
+    body: any;
     headers: object;
     incomingMessage: IncomingMessage;
 }
 
-export { ObjectSanitized };
 export default Request;
 
