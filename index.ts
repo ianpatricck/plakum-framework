21997e479afb3818eda644e8ac367f9759d468b5
ianpatricck <ianpatrick499@gmail.com>
2023-06-24 15:41:08 -0300

    feat: exporting Router

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import Plakum from "./plakum";
+import Router from './router';
 import Request from './http/request';
 import Response from './http/response';
 
 export default Plakum;
-export { Request, Response };
+export { Router, Request, Response };
