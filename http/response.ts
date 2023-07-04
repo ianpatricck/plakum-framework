4a18b0477e86418fd398937e7e7e2fcb6a102493
ianpatricck <ianpatrick499@gmail.com>
2023-07-04 15:13:28 -0300

    feat: setCors

--- http/response.ts
+++ http/response.ts
@@ -1,4 +1,10 @@
 import { ServerResponse } from "http";
+import { CORS, populateCorsBody } from './cors';
+
+/*
+ * Tipo padrão da resposta
+ *
+ */
 
 type Response = { 
     headers: object; 
@@ -9,9 +15,17 @@ type Response = {
     params: object;
     wasContentTypeSetted: boolean;
     setContentType: Function;
+    setHeader: Function;
+    setCors: Function;
     send: Function;
     serverResponse: ServerResponse;
 }
+
+/*
+ * Classe que fornece os métodos para manipular a
+ * resposta da requisição
+ *
+ */
 
 export class ResponseData {
 
@@ -34,10 +48,20 @@ export class ResponseData {
         this.serverResponse.setHeader('Content-Type', contentType);
     }
 
-    public send(data: JSON | string, statusCode: Response["statusCode"] = 200): void { 
-        
-        this.serverResponse.statusCode = statusCode;
+    public setHeader(key: string, value: string | number | readonly string[]): void {
+        this.serverResponse.setHeader(key, value);
+    }
 
+    public setCors(options: CORS): void {
+        const cors = populateCorsBody(options); 
+        for (const key in cors) {
+            this.setHeader(key, cors[key]);
+        }
+    }
+    
+    public send(data: JSON | string, statusCode: Response["statusCode"] = 200): void {        
+        this.serverResponse.statusCode = statusCode;
+ 
         if (this.wasContentTypeSetted) {
             this.serverResponse.end(data); 
         } else {
